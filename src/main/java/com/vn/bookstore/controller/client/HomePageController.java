d326a67ce5508c976c75c0c6dd704289ebf0cad5
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-08-08 20:08:28 +0700

    first commit

---
+++ src/main/java/com/vn/bookstore/controller/client/HomePageController.java
@@ -0,0 +1,13 @@
+package com.vn.bookstore.controller.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+    @GetMapping("/")
+    public String greeting(Model model) {
+        return "/client/homepage/show";
+    }
+}
